5dacfb72e3a0a2412117ad119e9ecf89df0b93c4
Gondi <berbagidown@gmail.com>
2024-08-19 22:46:23 +0700

    first commit

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello('Andi'));
+
+  var penjualan = barang('Rokok02', 091);
+  print(penjualan.barangId);
+  print(penjualan.jumlah);
+}
---
+++ lib/src/customer.dart
@@ -0,0 +1,8 @@
+class barang<X> {
+  X barangId;
+  X jumlah;
+
+  barang(this.barangId, this.jumlah);
+}
+
+class pembeli {}
